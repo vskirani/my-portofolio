b442d0a906bad16de1ba52e4b332ea5740e13e40
Dikocesrt <dikocesrt@gmail.com>
2026-02-06 21:37:59 +0700

    feat(project): added get project relations

--- src/dtos/project.dto.ts
+++ src/dtos/project.dto.ts
@@ -3,3 +3,30 @@ export type FindProjectsFilter = {
     isStarred?: boolean;
     limit?: number;
 };
+
+export type TechStackDTO = {
+    id: string;
+    name: string;
+};
+export type ProjectTypeDTO = {
+    code: string;
+    name: string;
+};
+export type ProjectDTO = {
+    id: string;
+    title: string;
+    description: string;
+    typeCode: string;
+    imageUrl: string;
+    githubUrl: string | null;
+    figmaUrl: string | null;
+    projectUrl: string | null;
+    otherUrl: string | null;
+    problem: string | null;
+    solution: string | null;
+    result: string | null;
+    isStarred: boolean;
+    createdAt: Date;
+    techStacks: TechStackDTO[];
+    projectType: ProjectTypeDTO;
+};
--- src/services/project.service.ts
+++ src/services/project.service.ts
@@ -1,36 +1,33 @@
 import { SuccessResponse } from "../dtos/response.dto";
-import { projects } from "../generated/prisma/client";
 import {
     findProjectByIDRepo,
     findProjectsRepo,
 } from "../repositories/project.repository";
-import { FindProjectsFilter } from "../dtos/project.dto";
+import { FindProjectsFilter, ProjectDTO } from "../dtos/project.dto";
 import { NotFoundError } from "../errors";
+import { toProjectDTO, toProjectDTOList } from "../mappers/project.mapper";
 
 export async function getListProjectsService(
     filter?: FindProjectsFilter,
-): Promise<SuccessResponse<projects[]>> {
+): Promise<SuccessResponse<ProjectDTO[]>> {
     const projects = await findProjectsRepo(filter);
-
     return {
         success: true,
         message: "Successfully retrieved list of projects",
-        data: projects,
+        data: toProjectDTOList(projects),
     };
 }
 
 export async function getProjectByIDService(
     id: string,
-): Promise<SuccessResponse<projects>> {
+): Promise<SuccessResponse<ProjectDTO>> {
     const project = await findProjectByIDRepo(id);
-
     if (!project) {
         throw new NotFoundError("Project not found");
     }
-
     return {
         success: true,
         message: "Successfully retrieved project by ID",
-        data: project,
+        data: toProjectDTO(project),
     };
 }
